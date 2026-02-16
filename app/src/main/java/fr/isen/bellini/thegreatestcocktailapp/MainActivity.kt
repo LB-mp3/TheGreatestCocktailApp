1021ff3b4a3225796ed30e2320373856cdfb588a
Loan BELLINI <loan.bellini@isen.yncrea.fr>
2026-02-16 10:33:47 +0100

    "Tests de prise en main"

--- app/src/main/java/fr/isen/bellini/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/fr/isen/bellini/thegreatestcocktailapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnit.Companion
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import fr.isen.bellini.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +40,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Loan",
+            color = Color.Magenta,
+            fontSize = 50.sp,
+            fontFamily = FontFamily.Cursive,
+        )
+
+    }
 }
+
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
