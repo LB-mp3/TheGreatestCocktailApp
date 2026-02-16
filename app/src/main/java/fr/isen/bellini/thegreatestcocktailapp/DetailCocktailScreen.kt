f78f19ff201ab255d1fef4d9c6b27293d10f96da
Loan BELLINI <loan.bellini@isen.yncrea.fr>
2026-02-16 10:57:44 +0100

    "Ajout du DetailCocktailScreen et connexion à MainActivity"

---
+++ app/src/main/java/fr/isen/bellini/thegreatestcocktailapp/DetailCocktailScreen.kt
@@ -0,0 +1,8 @@
+package fr.isen.bellini.thegreatestcocktailapp
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailCocktailScreen() {
+    Text("Detail Cocktail Screen")
+}
